73aa2c78eb33322d4a212f048035cf5bdf593a67
johannag285 <johannag285@gmail.com>
2019-06-20 11:08:55 -0500

    delete the path inicio and add new path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'inicio', loadChildren: './inicio/inicio.module#InicioPageModule' },
+
+
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    CommonModule,
+    RouterModule.forRoot(routes)
   ],
   exports: [RouterModule]
 })
